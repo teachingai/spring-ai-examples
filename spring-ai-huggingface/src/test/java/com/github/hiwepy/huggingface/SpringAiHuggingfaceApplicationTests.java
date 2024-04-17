9455b8c2170646aebfcac011e4979d5a09ea1373
wandl <hiwepy@163.com>
2024-04-17 22:29:33 +0800

    Spring AI 学习示例

---
+++ spring-ai-huggingface/src/test/java/com/github/hiwepy/huggingface/SpringAiHuggingfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hiwepy.huggingface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiHuggingfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-ai-qianfan/src/test/java/com/github/hiwepy/qianfan/SpringAiQianfanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hiwepy.qianfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiQianfanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
