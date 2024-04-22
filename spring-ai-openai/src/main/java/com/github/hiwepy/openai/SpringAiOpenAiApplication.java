d893dd8edb9fcccf434f2c26b1d3ebe0c6b2b4c3
wandl <hiwepy@163.com>
2024-04-22 22:01:42 +0800

    Spring AI 学习示例

---
+++ spring-ai-openai/src/main/java/com/github/hiwepy/openai/SpringAiOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.github.hiwepy.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOpenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiOpenAiApplication.class, args);
+    }
+
+}
