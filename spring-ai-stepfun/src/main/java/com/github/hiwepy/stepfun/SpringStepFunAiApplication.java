04270a9d33ee8ca483776094cb5e73a4b92238be
lujun <jun.lu@ncsi.com.cn>
2024-04-27 14:19:15 +0800

    feat:新增stepFun的示例工程

---
+++ spring-ai-stepfun/src/main/java/com/github/hiwepy/stepfun/SpringStepFunAiApplication.java
@@ -0,0 +1,16 @@
+package com.github.hiwepy.stepfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lujun
+ * @date 2024/4/27 13:29
+ */
+@SpringBootApplication
+public class SpringStepFunAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStepFunAiApplication.class, args);
+    }
+
+}
