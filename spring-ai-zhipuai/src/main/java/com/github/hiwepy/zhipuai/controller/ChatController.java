f62f3412c1a46c614a3809a575b4aea9ed1d7a35
wandl <hiwepy@163.com>
2024-05-12 22:56:06 +0800

    init: spring ai learn

--- spring-ai-zhipuai/src/main/java/com/github/hiwepy/zhipuai/controller/ChatController.java
+++ spring-ai-zhipuai/src/main/java/com/github/hiwepy/zhipuai/controller/ChatController.java
@@ -1,23 +1,20 @@
 package com.github.hiwepy.zhipuai.controller;
 
 import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.Generation;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.zhipuai.ZhipuAiChatClient;
-import org.springframework.ai.zhipuai.api.ZhipuAiChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 public class ChatController {
@@ -29,38 +26,22 @@ public class ChatController {
         this.chatClient = chatClient;
     }
 
+    @GetMapping("/v1/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+    @GetMapping("/v1/prompt")
+    public List<Generation> prompt(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        PromptTemplate promptTemplate = new PromptTemplate("Tell me a {adjective} joke about {topic}");
+        Prompt prompt = promptTemplate.create(Map.of("adjective", "funny", "topic", "cats"));
+        return chatClient.call(prompt).getResults();
+    }
+
     @PostMapping("/v1/chat/completions")
-    public Flux<ChatResponse> chatCompletions(@RequestBody ApiRecord.ChatCompletionRequest request) {
-        // 1、对Message进行转换
-        List<Message> messages = request.messages().stream().map(msg -> {
-            switch (msg.role()) {
-                case USER:
-                    return new UserMessage(msg.content());
-                case SYSTEM:
-                    return new SystemMessage(msg.content());
-                case ASSISTANT:
-                    return new AssistantMessage(msg.content());
-                default:
-                    return new SystemMessage(msg.content());
-            }
-        }).collect(Collectors.toList());
-        // 2、对ChatOptions进行转换
-        ChatOptions modelOptions = ZhipuAiChatOptions.builder()
-                .withModel(request.model())
-                .withMaxTokens(request.maxTokens())
-                .withDoSample(request.doSample())
-                .withTemperature(request.temperature())
-                .withTopP(request.topP())
-                .withUser(request.user())
-                .withStop(request.stop())
-                .build();
-        // 3、构建Prompt
-        Prompt prompt = new Prompt(messages, modelOptions);
-        // 4、调用ChatClient
-        if(Objects.nonNull(request.stream()) && Boolean.TRUE.equals(request.stream())){
-            return chatClient.stream(prompt);
-        }
-        return Flux.just(chatClient.call(prompt));
+    public Flux<ChatResponse> chatCompletions(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
     }
 
 }
