896ae6c40ef3de1ea1ecc500235bd0d9c6954616
wandl <hiwepy@163.com>
2024-06-23 23:31:29 +0800

    init: spring ai learn

--- spring-ai-ollama-prompt/src/main/java/com/github/teachingai/ollama/controller/ChatController.java
+++ spring-ai-ollama-prompt/src/main/java/com/github/teachingai/ollama/controller/ChatController.java
@@ -1,24 +1,16 @@
 package com.github.teachingai.ollama.controller;
 
-import com.github.teachingai.ollama.request.ApiRequest;
-import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.Generation;
-import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 public class ChatController {
@@ -30,41 +22,11 @@ public class ChatController {
         this.chatClient = chatClient;
     }
 
-    @GetMapping("/v1/generate")
-    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return Map.of("generation", chatClient.call(message));
-    }
-
     @GetMapping("/v1/prompt")
     public List<Generation> prompt(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         PromptTemplate promptTemplate = new PromptTemplate("Tell me a {adjective} joke about {topic}");
         Prompt prompt = promptTemplate.create(Map.of("adjective", "funny", "topic", "cats"));
         return chatClient.call(prompt).getResults();
-    }
-
-    @PostMapping("/v1/chat/completions")
-    public Flux<ChatResponse> chatCompletions(@RequestBody ApiRequest.ChatCompletionRequest chatRequest) {
-
-        chatRequest.messages().forEach(System.out::println);
-
-
-        ChatOptions modelOptions = new OllamaOptions()
-                .withModel(chatRequest.model())
-                .withTemperature(chatRequest.temperature()).withTopP(chatRequest.topP());
-
-        List<Message> messages = chatRequest.messages().stream().map(msg -> {
-            switch (msg.role()) {
-                case ASSISTANT:
-                    return new AssistantMessage(msg.content());
-                case SYSTEM:
-                    return new SystemMessage(msg.content());
-                default:
-                    return new UserMessage(msg.content());
-            }
-        }).collect(Collectors.toList());
-
-        Prompt prompt = new Prompt(messages, modelOptions);
-        return chatClient.stream(prompt);
     }
 
 }
