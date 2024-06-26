322e06b22c90ad4efa02d79c6dc7ced429da5abe
wandl <hiwepy@163.com>
2024-06-26 22:47:39 +0800

    init: spring ai learn

--- spring-ai-ollama-embedding/src/main/java/com/github/teachingai/ollama/controller/EmbeddingController.java
+++ spring-ai-ollama-embedding/src/main/java/com/github/teachingai/ollama/controller/EmbeddingController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,12 +24,12 @@ public class EmbeddingController {
 
     @GetMapping("/v1/embedding")
     public Map embedding(@RequestParam(value = "text") String text) {
-        return Map.of("embeddings", embeddingService.embedding(text));
+        return embeddingService.embedding(text);
     }
 
     @PostMapping("/v1/embedding")
-    public Map embedding(@RequestParam(value = "file") MultipartFile file) {
-        return Map.of("embeddings", embeddingService.embedding(file));
+    public List<Map> embedding(@RequestParam(value = "file") MultipartFile file) throws IOException {
+        return embeddingService.embedding(file);
     }
 
 }
