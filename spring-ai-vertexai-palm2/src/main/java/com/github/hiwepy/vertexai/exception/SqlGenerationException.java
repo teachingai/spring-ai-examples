0e02199401a719009d484a45be5dbcc15beafc8d
luffy <luffy@uzx.com>
2025-08-11 01:17:21 +0800

    init: spring ai learn

--- spring-ai-vertexai-palm2/src/main/java/com/github/hiwepy/vertexai/exception/SqlGenerationException.java
+++
@@ -1,7 +0,0 @@
-package com.github.hiwepy.vertexai.exception;
-
-public class SqlGenerationException extends RuntimeException {
-    public SqlGenerationException(String response) {
-        super(response);
-    }
-}
